e5869568c145ee4740972c68a10bf1be91a415b5
Damien Laureaux <damien@damien.timoa.lan>
2013-03-23 14:31:14 +0100

    Modification du contrôleur Event pour ajouter l'appel API

--- app/controllers/Event.js
+++ app/controllers/Event.js
@@ -3,11 +3,62 @@
  */
 var JUG = require('JUG');
 
+
+/*
+ * Mise à jour de la liste des événements
+ */
+function updateEvent(_data) {	
+	
+	var rows = [];
+	
+	for(dataId in _data) {
+		
+		// Mise à jour de l'événement courant (currentView)
+		if(dataId==0) {
+			$.titleLabel.text = _data[dataId].title;
+			$.dateLabel.text = _data[dataId].date;
+			$.descLabel.text = _data[dataId].description;
+		}	
+		
+		else {
+			// Création d'une ligne pour la liste à partir du contrôleur EventRow
+			var row = Alloy.createController('EventRow', {
+				title: _data[dataId].title,
+				date: _data[dataId].date,
+				description: _data[dataId].description,
+				rowId: _data[dataId].id
+			}).getView();
+			rows.push(row);
+			
+			row = null;
+		}
+						
+	}
+	
+	// Set the table
+	$.eventList.data = rows;
+};
+
+/*
+ * Chargement des événements
+ */
+if(Ti.Network.online) {
+		
+	JUG.httpRequest({
+		url: Alloy.CFG.JUGApi + "events/all.json",
+		format: "json",
+		callback: function(e) {
+			updateEvent(e);
+		}
+	});	
+}
+
+
 /*
  * /!\ Temporaire : Test de l'animation du menu en cliquant sur l'évenement courant (view en rouge) /!\
  */
 $.currentView.addEventListener("click", function(e) {
-	Ti.API.info("[---Event---] Menu click : " + Ti.App.Properties.getBool('menuOpen'));
+	Ti.API.info("[---Event---] Menu open : " + Ti.App.Properties.getBool('menuOpen'));
 	Ti.App.fireEvent("JUG:menuOpen");
 });
 
